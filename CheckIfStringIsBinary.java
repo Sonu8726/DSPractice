50bf88807e9a8bab8cb915f3af2e61fb1edaf4d8
Sonu Kumar <sonukumar4100.sk@gmail.com>
2022-12-29 22:30:23 +0530

    first commit

---
+++ CheckIfStringIsBinary.java
@@ -0,0 +1,23 @@
+public class CheckIfStringIsBinary {
+    public static void main(String[] args) {
+
+        System.out.println(checkBinary("Sonu Kumar"));
+        System.out.println(checkBinary("12345"));
+        System.out.println(checkBinary("10101"));
+        System.out.println(checkBinary("Sonu1011"));
+        System.out.println(checkBinary("10101010O"));
+    }
+
+    private static boolean checkBinary(String str) {
+        boolean status = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '0' || str.charAt(i) == '1') {
+                status = true;
+            } else {
+                status = false;
+                break;
+            }
+        }
+        return status;
+    }
+}
---
+++ ReplaceACharFromString.java
@@ -0,0 +1,25 @@
+public class ReplaceACharFromString {
+    public static void main(String[] args) {
+
+        String str = new String("Sonu Kumar on Fireozn");
+
+        ReplaceACharFromString rp = new ReplaceACharFromString();
+
+        System.out.println(str);
+
+        String str2 = rp.changeCharInString(str, 'o', 'm');
+
+        System.out.println(str2);
+    }
+
+    private String changeCharInString(String str, char oldCh, char newCh) {
+        StringBuffer sb = new StringBuffer(str);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == oldCh) {
+                sb.setCharAt(i, newCh);
+            }
+        }
+
+        return sb.toString();
+    }
+}
