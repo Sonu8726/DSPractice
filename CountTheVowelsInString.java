31be6ba48b069bd4820d12ed4fb6c90cf8fe2c57
Sonu Kumar <sonukumar4100.sk@gmail.com>
2022-12-29 22:50:43 +0530

    Added new file CountTheVowelsInString.java

---
+++ CountTheVowelsInString.java
@@ -0,0 +1,20 @@
+public class CountTheVowelsInString {
+    public static void main(String[] args) {
+        System.out.println(countTheVowelsInString("Sonu Kumar"));
+    }
+
+    private static int countTheVowelsInString(String str) {
+        int count = 0;
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a'
+                    || str.charAt(i) == 'e'
+                    || str.charAt(i) == 'i'
+                    || str.charAt(i) == 'o'
+                    || str.charAt(i) == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
